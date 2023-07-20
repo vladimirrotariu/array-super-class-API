7eb920d01908cd12541af56eee7c806bf94a5e3e
Vlad <rotariuvladimir@gmail.com>
2023-07-21 00:53:58 +0200

    Add find method for arrays

--- app/CodersArray.js
+++ app/CodersArray.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 class CodersArray {
   array;
   length;
@@ -7,7 +6,6 @@ class CodersArray {
     this.array = array;
 
     let incrementalLength = 0;
-    // eslint-disable-next-line no-unused-vars
     for (const value of array) {
       incrementalLength++;
     }
@@ -31,5 +29,13 @@ class CodersArray {
     }
 
     return false;
+  }
+
+  find(testFunction) {
+    for (let index = 0; index < this.length; index++) {
+      if (testFunction(this.array[index])) {
+        return this.array[index];
+      }
+    }
   }
 }
